7a0c92876bca19ee38a5032a5efd4dcdb710bf26
madman61rus <rhjnhjtn3yjhe>
2017-01-11 00:25:11 +0300

    - Исправил нарушения PEP8 -+ заменил domain_name.split('//')[1] на встроенную функцию urlparse -+ заменил многословный лишний кусок [line for line in file.read().splitlines()] на простое чтение из файла сразу в список - Убрал вывод в консоль в функции load_urls4check. Она и так возвращает None в случае неудаче. При выводе есть проверка

--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,13 @@
-
+import argparse
 
 def resize_image(path_to_original, path_to_result):
     pass
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", required=True, help="path to original image")
+    parser.add_argument("-w","--width", help="width of a new image")
+    parser.add_argument("-e","--height", help="height of a new image")
+    parser.add_argument("-s", "--scale", help="scale to a new image")
+    args = parser.parse_args()
